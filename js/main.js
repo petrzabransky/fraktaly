52404b53298f3ea4da33415c86c646b7354d88d6
Petr Zabransky <info@petrzabransky.cz>
2023-01-31 21:22:22 +0100

    Smooth scroll CSS without Javascript

--- js/main.js
+++ js/main.js
@@ -3,22 +3,6 @@ window.onload = function () {
     navMenu();
 }
 
-function smoothScroll() {
-    const links = document.querySelectorAll('a[href^="#"]');
-    for (let i = 0; i < links.length; i++) links[i].onclick = function () {
-        const target = document.querySelector(this.attributes.href.nodeValue),
-            targetY = target.offsetTop;
-        var speed, dir, step, dist, screenY,
-            anim = setInterval((function () {
-                screenY = document.documentElement.scrollTop + document.body.scrollTop,
-                    dir = screenY <= targetY ? 1 : -1,
-                    dist = Math.abs(screenY - targetY),
-                    (speed = dist / 10) < 1 && (speed = 1), step = dir * speed, dist < 1 ? clearInterval(anim) : scrollBy(0, step)
-            }), 10);
-        return !1
-    }
-}
-
 function navMenu() {
     const hamb = document.querySelector(".nav__hamb");
     const menu = document.querySelector(".nav__menu");
