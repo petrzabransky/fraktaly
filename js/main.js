78de7bd84f894ad296245afb924f34bf01af4a45
Petr Zabransky <info@petrzabransky.cz>
2023-02-06 07:35:18 +0100

    Remove console.log screen.width

--- js/main.js
+++ js/main.js
@@ -89,8 +89,6 @@ class Fractal {
     this.setCanvasSize();
     this.setElementParameters();
     this.setEvents();
-
-    console.log(this.element.clientWidth);
   }
 
   // Returns value adjusted for random variance
@@ -203,16 +201,16 @@ class Tree extends Fractal {
 
 // Navigation menu
 document.querySelector(".nav__hamburger").addEventListener("click", () => {
-    const menu = document.querySelector(".nav__menu");
-    const hamburger = document.querySelector(".nav__hamburger");
+  const menu = document.querySelector(".nav__menu");
+  const hamburger = document.querySelector(".nav__hamburger");
 
-    if (menu.classList.contains("nav__menu--active")) {
-        menu.classList.remove("nav__menu--active");
-        hamburger.src = "./img/ikony/hamburger.svg";
-    } else {
-        menu.classList.add("nav__menu--active");
-        hamburger.src = "./img/ikony/zavrit.svg";
-    }
+  if (menu.classList.contains("nav__menu--active")) {
+    menu.classList.remove("nav__menu--active");
+    hamburger.src = "./img/ikony/hamburger.svg";
+  } else {
+    menu.classList.add("nav__menu--active");
+    hamburger.src = "./img/ikony/zavrit.svg";
+  }
 });
 
 
