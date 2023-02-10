02f45077f8fe88baeffbf69715c12e071ce42f78
Petr Zabransky <info@petrzabransky.cz>
2023-02-10 08:52:14 +0100

    GULP - fix task for TS watch Modul Es2015

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,54 @@
 "use strict";
 
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps');
-const typescript = require('gulp-typescript');
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const concat = require("gulp-concat")
+const typescript = require("gulp-typescript");
+
+// 
+// Watch
+//
 
 const scss = (cb) => {
-  return gulp.src('./src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src("./src/scss/style.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./src/css'));
+    .pipe(gulp.dest("./src/css"));
   cb();
 };
 
-
 const ts = () => {
-
-  return gulp.src('src/ts/**/*.ts')
+  return gulp.src("src/ts/**/*.ts")
     .pipe(typescript({
-      noImplicitAny: true
-
+      noImplicitAny: true,
+      module: "ES2015"
     }))
-    .pipe(gulp.dest('./src/js'));
+    .pipe(gulp.dest("./src/js"));
 };
-
 
 exports.default = () => {
   gulp.watch("src/scss/**/*.scss", scss);
   gulp.watch("src/ts/**/*.ts", ts);
 };
-exports.ts = ts;
 
+
+// 
+// Distribution
+//
+
+const tsDist = () => {
+  return gulp.src([
+    "src/ts/**/*.ts",
+  ])
+    .pipe(typescript({
+      noImplicitAny: true,
+      module: "ES2015"
+    }))
+    .pipe(gulp.dest("./dist/js"));
+};
+
+exports.ts = tsDist;
 
 /*
 
