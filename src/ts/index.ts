7676bdbf3fe65c926733521b3ce48bf76280640d
Petr Zabransky <info@petrzabransky.cz>
2023-02-14 11:29:16 +0100

    Fix Main Menu - View Hamburger and Close icon

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -8,17 +8,24 @@ import { Tree } from "./modules/Tree";
 //
 
 // Navigation menu
-document.querySelector(".nav__hamburger").addEventListener("click", () => {
-	const menu = document.querySelector(".nav__menu");
-	const hamburger = document.querySelector(".nav__hamburger");
+const menu: Element = document.querySelector(".nav__menu");
+const hamburger: Element = document.querySelector(".nav__hamburger");
+const close: Element = document.querySelector(".nav__close");
 
-	if (menu.classList.contains("nav__menu--active")) {
-		menu.classList.remove("nav__menu--active");
-		hamburger.setAttribute("src", "./img/ikony/hamburger.svg");
-	} else {
-		menu.classList.add("nav__menu--active");
-		hamburger.setAttribute("src", "./img/ikony/zavrit.svg");
-	}
+console.log(menu);
+console.log(hamburger);
+console.log(close);
+
+hamburger.addEventListener("click", () => {
+	menu.classList.add("nav__menu--active");
+	hamburger.classList.add("d-none");
+	close.classList.remove("d-none");
+});
+
+close.addEventListener("click", () => {
+	menu.classList.remove("nav__menu--active");
+	close.classList.add("d-none");
+	hamburger.classList.remove("d-none");
 });
 
 //
